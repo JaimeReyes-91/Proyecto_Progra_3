ce0aebfa5239087dd52d171a6b601c06f2def941
Jaime Reyes-91 <jreyesr26@miumg.edu.gt>
2026-05-14 00:29:17 -0600

    Primera subida del proyecto por Jaime

--- src/main/java/com/tickets/util/CodigoTicketUtil.java
+++ src/main/java/com/tickets/util/CodigoTicketUtil.java
@@ -2,4 +2,7 @@ package com.tickets.util;
 
 public class CodigoTicketUtil {
 
+	public static String generarCodigo(int id) {
+		return String.format("TKT-%04d", id);
+	}
 }
--- src/main/java/com/tickets/util/ConexionDB.java
+++ src/main/java/com/tickets/util/ConexionDB.java
@@ -5,21 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
-    // ── Parámetros de conexión ────────────────────────────────────
-    // Formato: jdbc:postgresql://HOST:PUERTO/NOMBRE_BASE_DE_DATOS
-    private static final String URL  =
-        "jdbc:postgresql://localhost:5432/apirest_db";
+    
+    private static final String URL = "jdbc:postgresql://localhost:5432/apirest_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234"; 
  
-    private static final String USER     = "postgres";
-    private static final String PASSWORD = "1234"; // <-- cambia si es diferente
- 
-    /**
-     * Retorna una nueva conexión abierta a PostgreSQL.
-     * IMPORTANTE: el llamador es responsable de cerrarla con con.close().
-     * Siempre úsala dentro de un bloque try-with-resources:
-     *   try (Connection con = Conexion.obtener()) { ... }
-     */
-    public static Connection obtener() throws SQLException {
+    
+    public static Connection obtenerConexion() throws SQLException {
 	    try {
 	    	Class.forName("org.postgresql.Driver");
 	    }catch (ClassNotFoundException e) {
@@ -28,18 +20,15 @@ public class ConexionDB {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
  
-    /**
-     * Verifica si la conexión funciona. Llama este método UNA vez
-     * para confirmar que la configuración es correcta.
-     */
+   
     public static boolean probarConexion() {
-        try (Connection con = obtener()) {
-            System.out.println("✅ Conexión a PostgreSQL exitosa!");
-            System.out.println("   Base de datos: " + con.getCatalog());
+        try (Connection con = obtenerConexion()) {
+            System.out.println("Conexión a PostgreSQL exitosa!");
+            System.out.println("Base de datos: " + con.getSchema());
             return true;
         } catch (SQLException e) {
             System.err.println("❌ Error de conexión: " + e.getMessage());
-            System.err.println("   Verifica: PostgreSQL activo, credenciales, puerto 5432");
+            System.err.println("Verifica: PostgreSQL activo, credenciales, puerto 5432");
             return false;
         }
     }
