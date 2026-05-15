f02ee0b3857f8a43939f538509a01789aa13c2b8
JeinDR <jdepazr9@miumg.edu.gt>
2026-05-15 04:33:42 -0600

    Avances timeline

--- src/main/java/com/tickets/dao/TimeLineDAO.java
+++ src/main/java/com/tickets/dao/TimeLineDAO.java
@@ -1,5 +1,39 @@
 package com.tickets.dao;
 
-public class TimeLineDAO {
+import com.tickets.util.ConexionDB;
+import com.tickets.modelo.LineaTiempoEvento;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+public class TimeLineDAO {
+	
+	public void registrar(LineaTiempoEvento timeline)
+		throws Exception{
+		String sql = """
+                INSERT INTO ticket_timeline(
+                ticket_id,
+                actor_id,
+                estado,
+                observacion
+                )
+                VALUES (?, ?, ?, ?)
+                """;
+		
+		try (
+                Connection con = ConexionDB.obtenerConexion();
+                PreparedStatement ps = con.prepareStatement(sql)
+        ) {
+
+            ps.setInt(1, timeline.getTicketId());
+            ps.setInt(2, timeline.getActorId());
+            ps.setString(3, timeline.getEstado());
+            ps.setString(4, timeline.getObservacion());
+
+            ps.executeUpdate();
+        }
+    }
 }
+
